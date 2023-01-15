93e03a8fd36dd2316e39a81246ff2d15c2d952d4
Yauhen <dasdasfyx@gmail.com>
2023-01-15 12:05:10 +0300

    Version 0.1. Создал функцию FillStartArray

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿void FillStartArray(string[] stringArray, int maxLength, out int finalArraySize)
+{
+    finalArraySize = 0;
+
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write("Введите текстовый элемент массива [" + i + "] : ");
+        stringArray[i] = Console.ReadLine();
+
+        if (stringArray[i].Length <= maxLength)
+            finalArraySize++;
+    }
+}
